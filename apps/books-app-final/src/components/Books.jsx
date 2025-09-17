1d3fe4190627d37ea55303b34d509dc816a513d6
Mateusz Jabłoński <mail@mateuszjablonski.com>
2025-09-17 16:17:35 +0200

    task 7 solution

--- apps/books-app-final/src/components/Books.jsx
+++ apps/books-app-final/src/components/Books.jsx
@@ -1,16 +1,30 @@
+import { useState } from "react"
 import { Book } from "./Book"
 
 export const Books = ({ books }) => {
+    const [isTooHeight, setIsTooHeight] = useState(false);
+    const checkBooksHeight = (element) => {
+        if (element) {
+            if (element.getBoundingClientRect().height > 500) {
+                setIsTooHeight(true);
+            }
+        }
+    }
+
     return (
-        <ul>
-            {books.map((book) => (
-                <Book
-                    key={book.title}
-                    title={book.title}
-                    author={book.author} 
-                    publicationDate={book.publicationDate}
-                />
-            ))}
-        </ul>
+        <div>
+            {isTooHeight && <p style={{ color: 'red' }}>Masz masę książek!</p>}
+            {!isTooHeight && <p style={{ color: 'green' }}>Zbieraj dalej</p>}
+            <ul ref={element => checkBooksHeight(element)}>
+                {books.map((book) => (
+                    <Book
+                        key={book.title}
+                        title={book.title}
+                        author={book.author} 
+                        publicationDate={book.publicationDate}
+                    />
+                ))}
+            </ul>
+        </div>
     )
 }
