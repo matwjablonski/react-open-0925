d56d6f81e8da74eed66e92bc621b54f7f2c585d8
Mateusz Jabłoński <mail@mateuszjablonski.com>
2025-09-18 09:40:51 +0200

    task 11

--- apps/books-app-final/src/App.jsx
+++ apps/books-app-final/src/App.jsx
@@ -3,8 +3,13 @@ import Header from './components/Header'
 import { Books } from './components/Books'
 import { Footer } from './components/Footer'
 import { books } from './data'
+import { useEffect } from 'react'
 
 function App() {
+
+  useEffect(() => {
+    document.title = `Twój zbiór książek liczy ${books.length} książek`;
+  }, []);
 
   return (
    <div>
