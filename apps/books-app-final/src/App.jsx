93c66ce6d798e86df122ef325fdabfed6f26ddf4
Mateusz Jabłoński <mail@mateuszjablonski.com>
2025-09-18 10:40:24 +0200

    12

--- apps/books-app-final/src/App.jsx
+++ apps/books-app-final/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import { Books } from './components/Books'
 import { Footer } from './components/Footer'
 import { books } from './data'
+import { Suspense } from 'react'
 
 function App() {
 
